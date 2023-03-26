a663d0ac1b8027a0be8ddf895d54c53ee12f28c6
kylemcd <hello@kylemcd.com>
2023-03-26 13:42:45 -0500

    📦 NEW: Fix components

--- app/posts/[slug]/mdx-content.jsx
+++ app/posts/[slug]/mdx-content.jsx
@@ -2,15 +2,26 @@
 import React from 'react';
 import { MDXRemote } from 'next-mdx-remote';
 
-import { Heading } from '@/components/global/Typography';
+import { Heading, Paragraph } from '@/components/global/Typography';
 
-const H1 = (children) => {
-    <Heading size="lg" color={'--primary-font-color'} element="h1">
-        {children}
-    </Heading>;
+const H1 = ({ children }) => {
+    return (
+        <Heading size="lg" color={'--primary-font-color'} element="h1">
+            {children}
+        </Heading>
+    );
+};
+
+const P = ({ children, ...otherProps }) => {
+    return (
+        <Paragraph size="lg" color={'--primary-font-color'} {...otherProps}>
+            {children}
+        </Paragraph>
+    );
 };
 const MdxComponents = {
     h1: H1,
+    p: P,
 };
 
 export function MdxContent({ source }) {
