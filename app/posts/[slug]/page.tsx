50bc7eff4aee84644ba22be1b62713c673caca38
kylemcd <hello@kylemcd.com>
2023-03-26 12:28:17 -0500

    🐛 FIX: try this

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs';
+import fs, { promises as fsPromises } from 'fs';
 import path from 'path';
 import { serialize } from 'next-mdx-remote/serialize';
 import { Frontmatter, Post } from '@/types/posts';
@@ -8,7 +8,7 @@ async function getPost(filepath: string): Promise<Post<Frontmatter>> {
     const post = path.join(process.cwd(), 'posts' + filepath);
 
     // Read the file from the filesystem
-    const raw = await fs.readFile(post, 'utf-8');
+    const raw = await fsPromises.readFile(post, 'utf-8');
     // Serialize the MDX content and parse the frontmatter
     const serialized = await serialize(raw, {
         parseFrontmatter: true,
@@ -24,9 +24,22 @@ async function getPost(filepath: string): Promise<Post<Frontmatter>> {
     };
 }
 
+const fetchFiles = async () => {
+    const dir = path.resolve('./posts');
+
+    const filenames = fs.readdirSync(dir);
+
+    const markdownFiles = filenames.map((name) => path.join('/', dir, name));
+
+    console.log('Posts Brought In: ', markdownFiles);
+    // res.statusCode = 200;
+    // res.json(markdownFiles);
+};
+
 // import Test from '../../../posts/avoiding-burnout'
 
 const Post = async ({ params }: { params: any }) => {
+    await fetchFiles();
     const { serialized, frontmatter } = await getPost(`/${params.slug}/index.md`);
 
     return (
