5b79a6e1f539a74d96b1e887a5564aee70d9009b
kylemcd <hello@kylemcd.com>
2023-03-26 12:43:07 -0500

    🐛 FIX: typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 async function fetchPosts() {
     const apiUrl = () => {
-        if (process.env.NDOE_ENV === 'production') {
+        if (process.env.NODE_ENV === 'production') {
             return `https://${process.env.VERCEL_URL}`;
         }
         return `http://localhost:3000`;
