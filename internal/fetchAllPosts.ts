5c327c331d0757d6ea50ed1b55d186f7022362cb
kylemcd <hello@kylemcd.com>
2023-04-09 18:20:11 -0500

    🐛 FIX: Order posts by date

--- internal/fetchAllPosts.ts
+++ internal/fetchAllPosts.ts
@@ -2,7 +2,7 @@ import { allPosts } from 'contentlayer/generated';
 import { ContentLayerPost } from '@/types/posts';
 
 const fetchAllPosts = (): ContentLayerPost[] => {
-    return allPosts;
+    return allPosts.sort((a, b) => (a.date > b.date ? -1 : 1));
 };
 
 export default fetchAllPosts;
