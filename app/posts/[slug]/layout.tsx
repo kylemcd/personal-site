28426e730775ff8456f4d61cbb0bedafc89bbc93
Kyle McDonald <hello@kylemcd.com>
2023-08-12 16:00:58 -0500

    🐛 FIX: type error

--- app/posts/[slug]/layout.tsx
+++ app/posts/[slug]/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { Metadata, ResolvingMetadata } from 'next';
-import  fetchOnePost  from '@/internal/fetchOnePost';
+import fetchOnePost from '@/internal/fetchOnePost';
 
 import { cookies } from 'next/headers';
 import '@/app/globals.css';
@@ -67,7 +67,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
             <path class="font" d="M36.0682 100V30.1818H50.8295V60.9659H51.75L76.875 30.1818H94.5682L68.6591 61.4432L94.875 100H77.2159L58.0909 71.2955L50.8295 80.1591V100H36.0682Z"/>
             </svg>`,
         },
-        openGraph: { images: [`/og${post.title ? `?title=${post.title}` : ''}`] },
+        openGraph: { images: [`/og${post?.title ? `?title=${post.title}` : ''}`] },
     };
 }
 
