a7111651b9764eac2046c1cd73ee3652b4822f86
Kyle McDonald <hello@kylemcd.com>
2023-08-12 16:09:29 -0500

    🐛 FIX: Smaller bg image

--- app/posts/[slug]/layout.tsx
+++ app/posts/[slug]/layout.tsx
@@ -67,7 +67,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
             <path class="font" d="M36.0682 100V30.1818H50.8295V60.9659H51.75L76.875 30.1818H94.5682L68.6591 61.4432L94.875 100H77.2159L58.0909 71.2955L50.8295 80.1591V100H36.0682Z"/>
             </svg>`,
         },
-        openGraph: { images: [`/og${post?.title ? `?title=${post.title}` : ''}`] },
+        openGraph: { images: [`/opengraph-image${post?.title ? `?title=${post.title}` : ''}`] },
     };
 }
 
