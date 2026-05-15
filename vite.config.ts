0f6e072a1139b791cb803a6f42b2d256aa3268b9
Kyle McDonald <hello@kylemcd.com>
2026-05-15 12:24:33 -0500

    feat: dev server run on dev box

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,26 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const config = defineConfig(() => {
-	const isTest = process.env.VITEST === "true";
-	const alias = { "@": fileURLToPath(new URL("./src", import.meta.url)) };
+const config = defineConfig(({ mode }) => {
+  const isTest = process.env.VITEST === "true";
+  const alias = { "@": fileURLToPath(new URL("./src", import.meta.url)) };
 
-	return {
-		plugins: [
-			!isTest &&
-				cloudflare({
-					viteEnvironment: { name: "ssr" },
-					remoteBindings: process.env.CLOUDFLARE_REMOTE_BINDINGS === "1",
-				}),
-			tailwindcss(),
-			tanstackStart(),
-			viteReact(),
-		].filter(Boolean),
-		resolve: { alias },
-	};
+  return {
+    server: {
+      allowedHosts: mode === "development" ? true : [],
+    },
+    plugins: [
+      !isTest &&
+        cloudflare({
+          viteEnvironment: { name: "ssr" },
+          remoteBindings: process.env.CLOUDFLARE_REMOTE_BINDINGS === "1",
+        }),
+      tailwindcss(),
+      tanstackStart(),
+      viteReact(),
+    ].filter(Boolean),
+    resolve: { alias },
+  };
 });
 
 export default config;
